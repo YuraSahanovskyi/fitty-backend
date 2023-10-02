f871e81831ab04edc27b33401cb2c957bd394b3c
YuraSahanovskyi <yurasahanovskyi@gmail.com>
2023-10-02 10:08:32 +0300

    Basic http server

--- main.go
+++ main.go
@@ -6,11 +6,9 @@ import (
 )
 
 func main() {
-	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("it works"))
 	})
-	http.Handle("/test", fn)
-
 	log.Println("server started at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
